5ffa9f401e1d19b7f53752f3c1689ecfdbfaefad
itkin <nicolas.papon@webflows.fr>
2019-10-03 17:00:16 +0200

    boostrap-saas 3 tag v3.3.2+4 with -3 namespace

--- lib/bootstrap-sass/engine.rb
+++ lib/bootstrap-sass/engine.rb
@@ -5,7 +5,7 @@ module Bootstrap
         %w(stylesheets javascripts fonts images).each do |sub|
           app.config.assets.paths << root.join('assets', sub)
         end
-        app.config.assets.precompile << %r(bootstrap/glyphicons-halflings-regular\.(?:eot|svg|ttf|woff)$)
+        app.config.assets.precompile << %r(bootstrap-3/glyphicons-halflings-regular\.(?:eot|svg|ttf|woff)$)
       end
     end
   end
